11fd5d4837ae33df690beae47828505f84e71915
Chukwudi Nwachukwu <hsysinfo@gmail.com>
2017-08-19 22:53:52 +0100

    pushed the actual code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # beautify-json
-Dynamically add a file from a directory and use it in node
+Dynamically add a file from a directory and use it in nodejs application
+
+* On a terminal or command line: _cd_ into the directory that contains _spill_out.js_
+* ```node spill_out.js```
---
+++ spill_out.js
@@ -0,0 +1,9 @@
+// dynamically add a file, including its parent directory
+
+var path = require('path');
+var beautify = require(path.join(__dirname, 'jsfile', 'beautify.js'));
+var data_in = "{'name':'Chukwudi Nwachukwu', 'bio':{'nickname':'ichux', 'twitter':'0b1101111', 'bitbucket':'ichux'}}";
+
+console.log(beautify.js_beautify(data_in));
+console.log(beautify.js_beautify(data_in, {'indent_size': 1, 'indent_char': ' '}));
+console.log(beautify.js_beautify(data_in, {'indent_size': 1, 'indent_char': '\t'}));
